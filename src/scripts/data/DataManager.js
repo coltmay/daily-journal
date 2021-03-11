2664d82144b04f1bda15d46fb550b8cf886401a0
ColtMay <colten_mayberry@pm.me>
2021-03-11 11:22:04 -0600

    added mood filtering

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,4 +1,20 @@
+// Fetches data from API, waits until all data has been fetched, then returns in .json format.
+
+/*
+* json-server -p 8090 -w JournalData.json
+*/
+
+let entryCollection = [];
+
 export const getJournalEntries = () => {
     return fetch("http://localhost:8090/entries")
         .then(response => response.json())
+        .then(parsedResponse => {
+            entryCollection = parsedResponse;
+            return parsedResponse;
+        })
+}
+
+export const useEntryCollection = () => {
+    return [...entryCollection];
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,58 @@
-import { EntryListComponent } from "./entry/JournalEntryList.js"
+import { useEntryCollection } from "./data/DataManager.js";
+import { entryListComponent, filteredEntryListComponent } from "./entry/JournalEntryList.js"
 
-EntryListComponent();
+const applicationElement = document.querySelector("body");
+
+const clickSaveButton = () => {
+    applicationElement.addEventListener("click", event => {
+        if (event.target.id === "saveButton") {
+            console.log("Save")
+        }
+    })
+}
+
+const clickEditButton = () => {
+    applicationElement.addEventListener("click", event => {
+        if (event.target.id === "editButton") {
+            console.log("Edit")
+        }
+    })
+}
+
+const changeMoodSelector = () => {
+    applicationElement.addEventListener("change", event => {
+        if (event.target.id === "moodFilter") {
+            const mood = event.target.value;
+            showFilteredEntries(mood);
+        }
+    })
+}
+
+const showFilteredEntries = (mood) => {
+    // Filter by mood
+    //* Sets an array equal to the array that has been returned, but filtered.
+    const filteredEntries = useEntryCollection().filter(singleEntry => {
+        if (singleEntry.mood === mood) {
+            return singleEntry;
+        }
+    })
+
+    filteredEntryListComponent(filteredEntries);
+}
+
+const startJournal = () => {
+    entryListComponent();
+    clickSaveButton();
+    clickEditButton();
+    changeMoodSelector();
+}
+
+startJournal();
+
+/*
+[X] Add an element that will take the mood selector.
+[X] Create an empty array that will eventually hold the filtered entries.
+[X] Export a function that returns said array.
+[X] Set an event to do something based off a mood selecting element.
+[X] Tie the mood selecting element into the filtering function.
+*/
